ee1683e196aa48f17e209e4c1738d926e04ca198
MWLeonel <wmleonel6@gmail.com>
2020-09-05 09:12:14 -0300

    Cidades do Litoral

---
+++ Cidades/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#B");
+            Console.WriteLine("Barra do Turvo");
+            Console.WriteLine("Bertioga");
+            Console.Write("\n");
+
+            Console.WriteLine("#C");
+            Console.WriteLine("Cajati");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Cubatão");
+            Console.Write("\n");
+
+            Console.WriteLine("#E");
+            Console.WriteLine("Eldorado");
+            Console.Write("\n");
+
+            Console.WriteLine("#G");
+            Console.WriteLine("Guaruja");
+
+            Console.WriteLine("#I");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Itariri");
+            Console.WriteLine("Itanhaem");
+            Console.Write("\n");
+
+            Console.WriteLine("#J");
+            Console.WriteLine("Jacupiranga");
+            Console.WriteLine("Juquiá");
+            Console.Write("\n");
+
+            Console.WriteLine("#M");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Miracatu");
+            Console.Write("\n");
+
+            Console.WriteLine("#P");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Pedro de Toledo");
+            Console.Write("\n");
+
+            Console.WriteLine("#R");
+            Console.WriteLine("Registro");
+            Console.Write("\n");
+
+            Console.WriteLine("#S");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Sete Barras");
+        }
+    }
+}
